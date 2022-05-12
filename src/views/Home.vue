3f951048b6127e5e2716e7bc7dded6880e176d03
Rangga Arsy <arsyrangga@gmail.com>
2022-05-13 00:56:42 +0700

    update belajar vue js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,6 +26,7 @@ export default {
 
 <template>
   <div class="home px-6 py-4">
+    <!-- tombol untuk memanggil function increment -->
     <button
       @click="increment"
       class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
@@ -33,7 +34,20 @@ export default {
       Count+
     </button>
     <h3>Home</h3>
+
+    <!-- Data count -->
     <h3>{{ count }}</h3>
+
+    <!-- buat ternari operation render -->
+    <div
+      v-html="
+        products.length > 0
+          ? '<h3>Teks Ini Muncul setelah data muncul</h3>'
+          : ''
+      "
+    ></div>
+
+    <!-- buat Mapping Array -->
     <div v-for="product in products" :key="product.id">
       {{ product.title }}
     </div>
